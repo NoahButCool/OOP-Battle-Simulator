c544321f359b03aa5770ab53dd333fbaff4dd3b3
NoahButCool <noah.nde9@gmail.com>
2025-09-03 08:51:30 -0500

    Added Hero

--- hero.py
+++ hero.py
@@ -1,3 +1,5 @@
+import random
+
 class Hero:
     """
     This is our hero blueprint.
@@ -13,16 +15,18 @@ class Hero:
     """
     
     def __init__(self, name):
-        #TODO Set the hero's name.
-        #TODO Set the hero's health. You might give the hero more health than a goblin.
-        #TODO Set the hero's attack power. Should it be more consistent than the goblin's?
+        self.name = name
+        self.health = 250
+        self.attack_power = 20
     
 
     def strike(self):
-        # TODO Implement the hero's attack logic. It could be stronger or more consistent than a goblin's.
+        return random.randint(10, self.attack_power)
     
     def receive_damage(self, damage):
-        # TODO Implement take_damage
-        # TODO We should prevent health from going into the NEGATIVE
-    
-    #TODO define is_alive
+            self.health -= damage
+            if self.health < 0:
+                 self.health = 0
+            
+    def is_alive(self):
+        return self.health > 0
